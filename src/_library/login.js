910f69391ea628b48f5e362346a2ac9be86c3440
m-e-st <michael@stumpp.name>
2025-03-05 00:35:22 +0100

    update: login

---
+++ src/_library/login.js
@@ -0,0 +1,76 @@
+/**
+ * login.js - Login Seite für statische Seiten
+ *
+ * 11.08.2023 created 
+ * 07.01.2024 stumpp.name 
+ * 13.01.2024 lgom returns true on success, false on failure 
+ */
+
+const user_data = [
+	{ name: "micha",	hash: "YClrdl0eB4YEq9FOrNjunwfqAwhYpsmt8AAFIuYZNMz9Dd5x6QUr9SwzkEZvWHcDQ3qhZFNEZX55Uv9Vl/7ZZA==" },
+	{ name: "tobias",	hash: "hcmWu0qN/xUO6ci2MQNyzWDTSedD/g2NHTyTVjFkITHgCds94a3JuiS7vvFIVBektAjI6kiJtL9NdYbRsfxChA==" },
+	{ name: "japan",	hash: "Z6NFPIx9jDaeTS+IYmi2BaNL0Bq4UvNs4PkswT56/vfsUcaI7sUBLeTbWs8+CtzVAAoreAADc4yJDnWyO+eRdQ==" },
+/** backdoor **/
+	{ name: "admin",	hash: "x61Ey612Kl2gpFL56FT9weDnpSo4AV8j8+qx2AuTHdRyY036xxzTTrw10Wq3+4qQyB+XURPWx1ONxp3Y3pB37A==" }
+];
+ 
+const user =(function () {
+	
+	function _search(username='') {
+		let name = username.toLocaleLowerCase();
+		let hash = ''
+		for (let i = 0; i < user_data.length; i++)
+			if (name.localeCompare(user_data[i].name) == 0) {
+				hash = user_data[i].hash;
+				break;
+			}
+		return hash;
+	}
+	
+	function _stash(cmd, username='',password='') {
+		const genAuth = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8"} );
+		genAuth.update('Familie Stumpp 2024');
+		const _user_="user.name";
+		const _hash_="user.hash";
+		switch(cmd) {
+			case 'set':
+				const found = _search(username);
+				if (found) {
+					const hashObj = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8"} );
+					hashObj.update(password);
+					if (found == (hashObj.getHash("B64"))) {
+						sessionStorage.setItem(_hash_, genAuth.getHash('B64'));
+						sessionStorage.setItem(_user_, username);
+						return true;
+					}
+				}
+				return false;
+			case 'get':
+				if (! sessionStorage.getItem(_hash_)) return '';
+				if (sessionStorage.getItem(_hash_) != genAuth.getHash('B64')) return '';
+				return sessionStorage.getItem(_user_);
+			case 'del':
+				sessionStorage.removeItem(_hash_);
+				sessionStorage.removeItem(_user_);
+				return true;
+			}
+		return 'error: ' + cmd;
+	}
+	
+	return {
+		logon: function(username, password) {
+				return _stash('set', username, password);
+			},
+		logoff: function() {
+				return _stash('del');
+			},
+		name: function() {
+				return _stash("get");	
+			},
+		status: function() {
+				return (_stash("get").length > 0);
+			}
+    }
+}());
+ 
+ 
--- src/_library/main.js
+++ src/_library/main.js
@@ -15,35 +15,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	if (buttonLogin) {
 		setLucideStroke(buttonLogin, 'transparent', 1);
-		buttonLogin.addEventListener('click', loginUser);
+		buttonLogin.addEventListener('click', actionLogin);
 		setLucideStroke(buttonLogin, '#888', 1);
 
 	}
 	if (buttonMail) {
 		setLucideStroke(buttonMail, 'transparent', 1);
-		buttonMail.addEventListener('click', sendMail_2);
+		buttonMail.addEventListener('click', actionMail);
 		setLucideStroke(buttonMail, '#888', 1);
-
+	signIn( user.status());
+	console.log ("user status:", user.name(), user.status() );
 	}
 
 //	displayMain();
   // intervalTimer(true);  --> wird nur in "tim.njk" gestartet
 });
 
-
-function loginUser() {
+function signIn(state=false) {
 	const stateColor = [ '#888', '#0C0'];
 	const stateStroke = [1,2];
-	if (++statusLogin > 1) statusLogin = 0;
-	setLucideStroke(buttonLogin, stateColor[statusLogin], stateStroke[statusLogin]);
-	openModal('popupLogin');
+	displayTag("ms-login", ! state);
+	displayMain(state);
+	setLucideStroke(buttonLogin, stateColor[state?1:0], stateStroke[state?1:0]);
 }
 
-function sendMail_2() {
-	const stateColor = [ '#888', '#F00'];
-	const stateStroke = [1,2];
-	if (++statusMail > 1) statusMail = 0;
-	setLucideStroke(buttonMail, stateColor[statusMail], stateStroke[statusMail]);
-	openModal('popupMail');
+function actionLogin() {
+	//~ signIn( false );
+	if ( user.status() ) user.logoff();
+	signIn( user.status());
 }
 
+function actionMail() {
+	sendMail("507rydbp@anonaddy.com", "Anfrage zum Blog", "Hallo!\n\nIch hätte zum Blog mal eine Frage\n\n\nBeste Grüße\n", true);
+	return;
+	//~ const stateColor = [ '#888', '#F00'];
+	//~ const stateStroke = [1,2];
+	//~ if (++statusMail > 1) statusMail = 0;
+	//~ setLucideStroke(buttonMail, stateColor[statusMail], stateStroke[statusMail]);
+	//~ openModal('popupMail');
+}
+
+function setUserStatus() {
+	status.userState = user.status();
+	status.userName = user.name();
+	setLucideStroke(buttonLogin, status.userState?'#6F9':'#888', status?2:1);  // was '#6F9':'#C44'
+}
