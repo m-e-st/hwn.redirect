dfdb7d1f5b6ca86f6e7f41e01a68ba972f416d72
m-e-st <michael@stumpp.name>
2025-03-04 09:27:33 +0100

    update: first obsidian tests

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,25 @@
-/** v2.0 - 2025-02-27 **/
+/** v2.1 - 2025-03-03 **/
 
 const htmlMinifier = require ('html-minifier-terser');
 const lucideIcons = require("@grimlink/eleventy-plugin-lucide-icons");
+const markdownItCallouts = require("markdown-it-obsidian-callouts");
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItCallouts));
+	
     eleventyConfig.addPlugin(lucideIcons);
   	eleventyConfig.addShortcode("anchor", function setAnchor(anchorName) { return `<a name="${anchorName}"><br><br><hr></a>`; });
-  	eleventyConfig.addFilter("Datum", function(value) { 
+  	eleventyConfig.addFilter("Datum", function(value) {
 		const string = value.toISOString();
 		return string.slice(8,10) +'.' + string.slice(5,7) + '.' + string.slice(0,4);
 	});
-  	eleventyConfig.addFilter("dateTitle", function(value) { 
+  	eleventyConfig.addFilter("dateTitle", function(value) {
 		const token = value.split('_')[1];
 		if (! token) return "ERROR 1";
 		const parts = token.split('-');
 		return parts[2] + '-' + parts[1] + '-' + parts[0];
 	});
-  	eleventyConfig.addFilter("Ascii", function(value) { 
+  	eleventyConfig.addFilter("Ascii", function(value) {
 		const originalChars = "äöüÄÖÜßÑñáéíóú";
 		const replacementChars = "aouAOUSNnaeiou";
 		let result = value;
@@ -24,11 +27,11 @@ module.exports = function (eleventyConfig) {
 			result = result.replaceAll(originalChars[i], replacementChars[i]);
 		return result;
 	});
-  	eleventyConfig.addFilter("Upperwords", function(value) { 
+  	eleventyConfig.addFilter("Upperwords", function(value) {
 		return value.replace(/\b\w/g, char => char.toUpperCase());
 		//~ return str.replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
 	});
-	
+
 	eleventyConfig.addTransform ('htmlMinifier', function(content,outputPath) {
 		if (	(process.env.ELEVENTY_ENV === 'production')
 			&&	(outputPath)
@@ -44,13 +47,16 @@ module.exports = function (eleventyConfig) {
 		}
 		return content;
 	});
-	
-  	
+
+
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static/**");
 	eleventyConfig.addPassthroughCopy("src/favicon.ico");
 	eleventyConfig.addPassthroughCopy("src/**/*.php");
-	
+	eleventyConfig.addPassthroughCopy("src/blog/**/*.png");
+	eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg");
+	eleventyConfig.addPassthroughCopy("src/blog/**/*.svg");
+
 	// This allows Eleventy to watch for file changes during local development.
 	//~ eleventyConfig.addWatchTarget("src/**/*.php");
 	eleventyConfig.addWatchTarget("src/_library/");
@@ -59,7 +65,7 @@ module.exports = function (eleventyConfig) {
 	return {
 		dir: {
 			input: "src/",
-			output: "public/",      
+			output: "public/",
 			includes: "_includes",
 			layouts: "_layouts"
 		},
---
+++ src/blog/Sandbox/BilderFabrik.md
@@ -0,0 +1,15 @@
+
+### internes Bild
+Hier wird ein internes Bild eingebunden:
+![CHERRY BLOSSOM](cherry-blossom-japan.png)
+
+> [!warning] Achtung
+> Damit dies funktioniert, muss Obsidian so eingestellt sein, dass die Attachments im selben Ordner wie der Text liegen. 
+> ![SCREENSHOT](screenshot-obsidian-settings-2.png)
+> Außerdem ist die Property **permalink** mit dem Inhalt `"{{ page.filePathStem }}.html"` zwingend erforderlich (inkl. der Hochkommata), damit das lokale Bild angezeigt wird.
+
+### externes Bild
+Dieses Bild wird direkt aus dem Internet geladen:
+ ![cherry blooming](https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/%E4%B8%80%E7%9B%AE%E5%8D%83%E6%9C%AC%E6%A1%9C_%28Cherry_blooming_view_from_Yoshimizu_Shrine%29_12_Apr%2C_2014_-_panoramio.jpg/1920px-%E4%B8%80%E7%9B%AE%E5%8D%83%E6%9C%AC%E6%A1%9C_%28Cherry_blooming_view_from_Yoshimizu_Shrine%29_12_Apr%2C_2014_-_panoramio.jpg) 
+ Dieses Bild wird über LoremPicsum geladen:
+ ![forest](https://picsum.photos/id/28/600/400)
